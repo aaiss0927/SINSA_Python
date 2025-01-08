5de6cf2d944968d1d671be6ac0d90caa32bb645d
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2025-01-08 15:34:15 +0900

    add inference.py

--- inference.py
+++ inference.py
@@ -3,11 +3,9 @@ from model import LinearRegression
 from train import Trainer
 
 def inference():
-    # 데이터 로드
     dataset = Dataset('data.txt')
     x_data, y_data = dataset.get_data()
     
-    # 모델 생성 및 학습
     model = LinearRegression()
     trainer = Trainer(model)
     trainer.train(x_data, y_data)
